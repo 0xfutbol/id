315408b7c9241cdb909a1b5f5c4a9c3496a31a5e
Agu Rodríguez <me@agu.uy>
2024-10-03 22:45:32 -0300

    Format

--- webapp/components/navbar.tsx
+++ webapp/components/navbar.tsx
@@ -19,11 +19,11 @@ export const Navbar = () => {
       <NavbarContent className="basis-1/5" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit p-2">
           <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Image 
-              alt="MetaSoccer" 
-              src="https://assets.metasoccer.com/metasoccer-logo.svg" 
-              height={24} 
-              width={120} 
+            <Image
+              alt="MetaSoccer"
+              src="https://assets.metasoccer.com/metasoccer-logo.svg"
+              height={24}
+              width={120}
               className="filter invert"
             />
           </NextLink>
