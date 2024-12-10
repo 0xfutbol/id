4900aeaac983072e702338dcefa637ae1a0e475f
Agu Rodríguez <me@agu.uy>
2024-12-09 23:33:08 -0300

    Add devsoccer config

--- webapp/config/apps.tsx
+++ webapp/config/apps.tsx
@@ -33,6 +33,34 @@ export const APP_CONFIG: Record<string, AppConfig> = {
       </>
     ),
   },
+
+  // MetaSoccer
+  DEVSOCCER: {
+    name: "DevSoccer",
+    logo: (
+      <img
+        alt="DevSoccer"
+        src={getImgUrl("https://assets.metasoccer.com/metasoccer-logo.svg")}
+        style={{ height: "40px", width: "auto", filter: "invert(1)" }}
+      />
+    ),
+    pre: (
+      <>
+        <p className="text-sm">Connect your MetaSoccer ID to start playing.</p>
+        <div className="flex items-start gap-2">
+          <p className="text-sm text-foreground-500">
+            MetaSoccer ID is your unique identifier in the MetaSoccer
+            World—think of it like your username for any MetaSoccer game.
+          </p>
+        </div>
+        <p className="text-sm">
+          Don’t have one yet? No worries! Just connect your wallet, and you’ll
+          be able to claim yours instantly.
+        </p>
+      </>
+    ),
+    redirectUri: "https://manag3r.devsoccer.com",
+  },
   LOCAL: {
     name: "MetaSoccer (Local)",
     logo: (
