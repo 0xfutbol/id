7e9a9b6dd02f05a2f5fa686dc6b00b5f679dba06
Agu Rodríguez <me@agu.uy>
2025-01-27 13:22:23 -0300

    Fix admin/account not returning associated TON address

--- server/src/routes/adminRoute.ts
+++ server/src/routes/adminRoute.ts
@@ -1,13 +1,13 @@
 import express from 'express';
 import { authenticateAdmin } from '../common/auth';
-import { getDiscordAccountByAddress, getReferralCount, getTonAccountByTonAddress } from '../repo/db';
+import { getDiscordAccountByAddress, getReferralCount, getTonAccountByAddress } from '../repo/db';
 
 const adminRouter = express.Router();
 
 adminRouter.get('/account/:address', authenticateAdmin, async (req: express.Request & { user?: { owner: string } }, res) => {
   try {
     const discordAccount = await getDiscordAccountByAddress(req.params.address);
-    const tonAccount = await getTonAccountByTonAddress(req.params.address);
+    const tonAccount = await getTonAccountByAddress(req.params.address);
     const referralCount = await getReferralCount(req.params.address);
 
     const info = {
