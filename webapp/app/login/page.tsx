669995791dd8245010cdf2eac6bf91ffa2864251
Agu Rodríguez <me@agu.uy>
2025-05-19 19:43:11 -0300

    Update page.tsx

--- webapp/app/login/page.tsx
+++ webapp/app/login/page.tsx
@@ -6,7 +6,7 @@ import { getImgUrl } from "@/utils/getImgUrl";
 export default function Login() {
   return (
     <div className="flex flex-col md:flex-row h-screen w-screen overflow-hidden">
-      <div className="h-screen w-full md:w-1/2">
+      <div className="w-full md:w-1/2">
         <img
           alt="0xFútbol Ecosystem"
           className="h-full w-full object-cover"
