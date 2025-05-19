69d2dea2241a7dedde1f71e8abd34a4655ec9e17
Agu Rodríguez <me@agu.uy>
2025-05-19 12:41:09 -0300

    Clean up

--- webapp/services/auth-service.ts
+++
@@ -1,5 +0,0 @@
-import { AuthService } from "@0xfutbol/id";
-
-import { API_CONFIG } from "@/config/api";
-
-export const authService = new AuthService(API_CONFIG.backendUrl);
--- webapp/services/index.ts
+++
@@ -1,7 +0,0 @@
-// Export services
-export * from './account-service';
-export * from './asset-service';
-export * from './auth-service';
-
-// Export Blockchain service types and helpers
-export * from './blockchain';
