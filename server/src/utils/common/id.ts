6a698c62bad129e955282a8529402a544e6af0cd
Agu Rodríguez <me@agu.uy>
2025-05-20 20:00:28 -0300

    Remove unnecessary escaping from messageTemplate in id.ts for improved environment variable handling.

--- server/src/utils/common/id.ts
+++ server/src/utils/common/id.ts
@@ -3,7 +3,7 @@ import { OxFutbolID } from "@0xfutbol/id-sign";
 console.log({
   chainId: parseInt(process.env.ONCHAIN_CHAIN_ID as string),
   contractAddress: process.env.ONCHAIN_CONTRACT_ADDRESS as string,
-  messageTemplate: process.env.ONCHAIN_MESSAGE_TEMPLATE?.replace('\\\\', '\\'),
+  messageTemplate: process.env.ONCHAIN_MESSAGE_TEMPLATE,
   jwtSecret: process.env.JWT_SECRET as string,
   privateKey: process.env.ONCHAIN_PRIVATE_KEY as string,
 });
@@ -11,7 +11,7 @@ console.log({
 export const oxFutboId = new OxFutbolID({
   chainId: parseInt(process.env.ONCHAIN_CHAIN_ID as string),
   contractAddress: process.env.ONCHAIN_CONTRACT_ADDRESS as string,
-  messageTemplate: process.env.ONCHAIN_MESSAGE_TEMPLATE?.replace('\\\\', '\\'),
+  messageTemplate: process.env.ONCHAIN_MESSAGE_TEMPLATE,
   jwtSecret: process.env.JWT_SECRET as string,
   privateKey: process.env.ONCHAIN_PRIVATE_KEY as string,
 });
