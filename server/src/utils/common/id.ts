8b07ffe2782858c2daded1c3baff440642af7142
Agu Rodríguez <me@agu.uy>
2025-05-20 20:32:55 -0300

    Add package.json and package-lock.json for project dependencies; introduce legacy ID signing support in id.ts for improved authentication flexibility.

--- server/src/utils/common/id.ts
+++ server/src/utils/common/id.ts
@@ -1,21 +1,20 @@
 import { OxFutbolID } from "@0xfutbol/id-sign";
+import { OxFutbolID as OxFutbolIDLegacy } from "@0xfutbol/id-sign-legacy";
 
-console.log({
+const legacyOxFutboId = new OxFutbolIDLegacy({
   chainId: parseInt(process.env.ONCHAIN_CHAIN_ID as string),
   contractAddress: process.env.ONCHAIN_CONTRACT_ADDRESS as string,
-  messageTemplate: process.env.USE_METASOCCER_SIGNATURE
-    ? 'Authenticate with MetaSoccerID\n\nID: {id}\n\nExpiration: {expiration}'
-    : undefined,
   jwtSecret: process.env.JWT_SECRET as string,
   privateKey: process.env.ONCHAIN_PRIVATE_KEY as string,
 });
 
-export const oxFutboId = new OxFutbolID({
+const modernOxFutboId = new OxFutbolID({
   chainId: parseInt(process.env.ONCHAIN_CHAIN_ID as string),
   contractAddress: process.env.ONCHAIN_CONTRACT_ADDRESS as string,
-  messageTemplate: process.env.USE_METASOCCER_SIGNATURE
-    ? 'Authenticate with MetaSoccerID\n\nID: {id}\n\nExpiration: {expiration}'
-    : undefined,
   jwtSecret: process.env.JWT_SECRET as string,
   privateKey: process.env.ONCHAIN_PRIVATE_KEY as string,
 });
+
+export const oxFutboId = process.env.USE_METASOCCER_SIGNATURE
+  ? legacyOxFutboId
+  : modernOxFutboId;
