3c03206922f427977a7a945781d07d3fafa954fa
Agu Rodríguez <me@agu.uy>
2025-05-20 20:07:32 -0300

    Update messageTemplate in oxFutboId initialization to conditionally use MetaSoccerID format based on environment variable, enhancing configuration flexibility.

--- server/src/utils/common/id.ts
+++ server/src/utils/common/id.ts
@@ -11,7 +11,9 @@ console.log({
 export const oxFutboId = new OxFutbolID({
   chainId: parseInt(process.env.ONCHAIN_CHAIN_ID as string),
   contractAddress: process.env.ONCHAIN_CONTRACT_ADDRESS as string,
-  messageTemplate: process.env.ONCHAIN_MESSAGE_TEMPLATE,
+  messageTemplate: process.env.USE_METASOCCER_SIGNATURE
+    ? 'Authenticate with MetaSoccerID\n\nID: {id}\n\nExpiration: {expiration}'
+    : undefined,
   jwtSecret: process.env.JWT_SECRET as string,
   privateKey: process.env.ONCHAIN_PRIVATE_KEY as string,
 });
